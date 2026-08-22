d0384d03e6a3a7961cfa59c6a59261359bb0ee99
Frédéric Delhoume <delhoume@gmail.com>
2026-08-22 16:50:40 +0200

    cli commands overide  defaults

--- tiffoftheday.js
+++ tiffoftheday.js
@@ -2,8 +2,6 @@ import sharp from "sharp"
 import { parseArgs } from "node:util"
 import fs from "node:fs"
 
-const validlevels = [];
-
 // poly is an array of [ x, y ] elements
 // range in percent ([0 1])
 function pointinpoly(x, y, poly)
@@ -51,24 +49,33 @@ const cli_config = {
         width: { type: 'string', default: "1874" }, // generated image width
         height: { type: 'string', default: "1440" }, // generated image heioht
         border: { type: 'string', default: "4" },  // overview border size
-        ovroffset: { type: 'string', default: "50" }, // overview topright offset
-        cropborder: { type: 'string', default: "5" }, // hidhlight border  size 
-        ovrratio: { type: 'string', default: "5" }, // gernerated iamge width to overiew widtg ratio
-        filtlvlmin: { type: 'string', default: "2" }, // minimum tiff level
-        filtlvlmax: { type: 'string', default: "256" }, // maximum tiff level
-        background: { type: 'string', default: "white" }, // borders color
+        level: { type: 'string', default: "-1" }, // selected level in image
+        x: { type: 'string', default: "-1" }, // x position in level imagee
+        y: { type: 'string', default: "-1" }, // y position in level image
+        overviewoffset: { type: 'string', default: "50" }, // overview topright offset
+        highlightborder: { type: 'string', default: "5" }, // highlight border  size 
+        overviewborder: { type: 'string', default: "5" }, // generated iamge width to overiew width ratio
+        overviewratio: { type: 'string', default: "5" }, // generated iamge width to overiew width ratio
+        levelratiomin: { type: 'string', default: "2" }, // minimum ratio between full and selected images
+        levelratiomax: { type: 'string', default: "256" }, // maximum ratio between full and selected images
+        overviewbordercolor: { type: 'string', default: "white" }, // borders color
+        hisghlightbordercolor: { type: 'string', default: "white" }, // borders color
         validregion: { type: 'string', default: "[[[0.0,0.0],[ 1.0,0.0], [1.0, 1.0],[0.0, 1.0]]]" }, // valid area polygons in %
-        verbose: { type: 'boolean', short: "v", default: false }
+        verbose: { type: 'boolean', short: "v", default: false },
+        notimestamp: { type: 'boolean', default: false }
     },
-    allowPositionals: true
+    allowPositionals: true,
+    tokens: true
 };
+
 
 let params = {};
 let verbose = false;
 
 function initParameters()
 {
-    const { values, positionals } = parseArgs(cli_config);
+    const { values, positionals, tokens } = parseArgs(cli_config);
+    console.log(tokens);
     params = values;
     verbose = getStringParameterValue('verbose');
     if (verbose) console.log(values, positionals);
@@ -81,6 +88,17 @@ function initParameters()
             params[name] = params_values[name];
         }
     }
+    console.log(params);
+    // second pass on token to overwrite with cli arguments 
+    //  --width 2000 -c congif.json alwas sets width to 2000
+    tokens.filter((token) => token.kind === 'option')
+        .forEach((token) =>
+        {
+            const name = token.name;
+            if (token.value)
+                params[name] = token.value;
+        });
+    console.log(params);
     if (positionals.length > 0)
         return positionals[0];
 
@@ -110,7 +128,7 @@ function isvalid(xpc, ypc)
         if (inside) insidearray.push(p);
         if (verbose) console.log("polygon", poly, inside);
     }
-    if (verbose && insidearray.length == 0)  console.log("rejected");
+    if (verbose && insidearray.length == 0) console.log("rejected");
     return insidearray.length > 0;
 }
 
@@ -121,22 +139,24 @@ async function tiffoftheday()
     if (verbose) console.log(imageinfo)
     const pages = "pages" in imageinfo ? imageinfo.pages : 0;
     const validlevels = [];
-    let ovrwidth = Infinity;
-    let ovrlevel = 0;
+    const levels = [];
+    let overviewwidth = Infinity;
+    let overviewlevel = 0;
 
-    const border = getIntParameterValue('border');
-    const ovroffset = getIntParameterValue('ovroffset');
-    const ovrratio = getIntParameterValue('ovrratio');
-    const cropborder = getIntParameterValue('cropborder');
-    const filtlvlmin = getIntParameterValue('filtlvlmin');
-    const filtlvlmax = getIntParameterValue('filtlvlmax');
+    const overviewborder = getIntParameterValue('overviewborder');
+    const highlightborder = getIntParameterValue('highlightborder');
+    const overviewoffset = getIntParameterValue('overviewoffset');
+    const overviewratio = getIntParameterValue('overviewratio');
+    const levelratiomin = getIntParameterValue('levelratiomin');
+    const levelratiomax = getIntParameterValue('levelratiomax');
     const outputwidth = getIntParameterValue('width')
     const outputheight = getIntParameterValue('height');
-    const background = getStringParameterValue('background')
-    if (pages < 1) {
+    const overviewbordercolor = getStringParameterValue('overviewbordercolor')
+    const highlighbordercolor = getStringParameterValue('highlightbordercolor')
+    if (pages == 0) {
         validlevels.push({ page: 0, width: imageinfo.width, height: imageinfo.height });
-        ovrlevel = 0;
-        ovrwidth = imageinfo.width;
+        overviewlevel = 0;
+        overviewwidth = imageinfo.width;
     } else {
         for (let currentpage = 0; currentpage < pages; currentpage++) {
             const pageinfo = await sharp(imagename, {
@@ -145,13 +165,15 @@ async function tiffoftheday()
             }).metadata();
             const lwidth = pageinfo.width;
             const lheight = pageinfo.height;
-            if (verbose) console.log("page", lwidth, lheight);
-            if ((lwidth >= (outputwidth * filtlvlmin)) && (lheight >= (outputheight * filtlvlmin))
-                && (lwidth <= (outputwidth * filtlvlmax)) && (lheight <= (outputheight * filtlvlmax))) {
-                validlevels.push({ page: currentpage, width: lwidth, height: lheight });
-                if (lwidth < ovrwidth) {
-                    ovrwidth = lwidth;
-                    ovrlevel = validlevels.length - 1;
+            let isvalid = (lwidth >= (outputwidth * levelratiomin)) && (lheight >= (outputheight * levelratiomin))
+                && (lwidth <= (outputwidth * levelratiomax)) && (lheight <= (outputheight * levelratiomax))
+            levels.push({ page: currentpage, width: lwidth, height: lheight, valid: isvalid });
+            if (isvalid) {
+                validlevels.push(currentpage);
+                // find smallest of valid pages
+                if (lwidth < overviewwidth) {
+                    overviewwidth = lwidth;
+                    overviewlevel = currentpage;
                 }
             }
         }
@@ -161,16 +183,22 @@ async function tiffoftheday()
         process.exit(1);
     }
 
-    if (verbose) console.log("ovr", ovrlevel, ovrwidth);
+    if (verbose) console.log("levels", levels);
+    if (verbose) console.log("valid levels", validlevels);
+    if (verbose) console.log("overview level", overviewlevel, levels[overviewlevel]);
 
-    const level = validlevels[Math.floor(Math.random() * validlevels.length)];
-    if (verbose) console.log("selected level", level);
-    const page = level.page;
-    const width = level.width;
-    const height = level.height;
-
+    let levelidx = getIntParameterValue("level")
+    if (levelidx == -1)
+        levelidx = validlevels[Math.floor(Math.random() * validlevels.length)];
+     const level = levels[levelidx];
+    if (verbose) console.log("selected level", levelidx, level);
+  if (!validlevels.includes(levelidx)) {
+        console.log("selected level does not respect constraints");
+        process.exit(1)
+    }
     let x, y, xpc, ypc;
 
+    if (verbose) console.log("Selecting valid area");
     do {
         x = Math.floor(outputwidth / 2 + Math.random() * (level.width - outputwidth));
         y = Math.floor(outputheight / 2 + Math.random() * (level.height - outputheight));
@@ -180,18 +208,29 @@ async function tiffoftheday()
         }
         xpc = x / level.width;
         ypc = y / level.height;
-        if (verbose) console.log("coords in level", xpc, ypc);
+        if (verbose) console.log("tring", x, y, "(", Math.round(xpc * 100) / 100, Math.round(ypc * 100) / 100., "%)");
     } while (!isvalid(xpc, ypc));
 
-    const overview_original_ratio = validlevels[ovrlevel].width / validlevels[ovrlevel].height;
-    const new_overview_width = Math.floor(outputwidth / ovrratio);
+    let xorig = getIntParameterValue("x");
+    let yorig = getIntParameterValue("y");
+
+    if (xorig != -1) x = xorig;
+    if (yorig != -1) y = yorig;
+
+    xpc = x / level.width;
+    ypc = y / level.height;
+
+
+    const overview_original_ratio = levels[overviewlevel].width / levels[overviewlevel].height;
+    const new_overview_width = Math.floor(outputwidth / overviewratio);
     const new_overview_height = Math.floor(new_overview_width / overview_original_ratio);
 
+    if (verbose) console.log("generating overview");
     const overview = await sharp(imagename, {
         limitInputPixels: false,
-        page: ovrlevel
+        page: overviewlevel
     }).resize(new_overview_width, new_overview_height)
-        .extend({ left: border, right: border, top: border, bottom: border, background: background })
+        .extend({ left: overviewborder, right: overviewborder, top: overviewborder, bottom: overviewborder, background: overviewbordercolor })
         .toFile("overview.jpg");
 
     const cropzone = {
@@ -200,7 +239,7 @@ async function tiffoftheday()
         width: outputwidth,
         height: outputheight
     };
-
+    if (verbose) console.log("extracting selected detail image");
     const cropwidth = Math.floor((cropzone.width / level.width) * new_overview_width);
     const cropheight = Math.floor((cropzone.height / level.height) * new_overview_height);
     const cropx = Math.floor((cropzone.left / level.width) * new_overview_width);
@@ -214,24 +253,26 @@ async function tiffoftheday()
         .extract(cropzone)
         .toFile("extract.jpg");
 
-    if (verbose) console.log("crop", cropx, cropy, cropwidth, cropheight);
+    if (verbose) console.log("generating highlight in overview");
+    if (verbose) console.log("highlight location in overview", cropx, cropy, cropwidth, cropheight);
     const crophighlight = await sharp("extract.jpg")
         .resize(cropwidth, cropheight)
         // .tint('red')
-        .extend({ left: cropborder, right: cropborder, top: cropborder, bottom: cropborder, background: background })
+        .extend({ left: highlightborder, right: highlightborder, top: highlightborder, bottom: highlightborder, background: highlighbordercolor })
         .toFile("highlight.jpg");
-
-    const generated_name = `tiffoftheday_${Math.floor(Date.now() / 1000)}.jpg`;
+    if (verbose) console.log("compositing final image");
+    const suffix = getStringParameterValue('notimestamp') ? "" : `_${Math.floor(Date.now() / 1000)}`;
+    const generated_name = `tiffoftheday${suffix}.jpg`;
     const finalimage = await sharp("extract.jpg")
         .composite([{
             input: "overview.jpg",
-            top: ovroffset - border,
-            left: outputwidth - new_overview_width - ovroffset - border
+            top: overviewoffset - overviewborder,
+            left: outputwidth - new_overview_width - overviewoffset - overviewborder
         },
         {
             input: "highlight.jpg",
-            top: ovroffset + cropy - cropborder,
-            left: cropx - cropborder + outputwidth - new_overview_width - ovroffset
+            top: overviewoffset + cropy - highlightborder,
+            left: cropx - highlightborder + outputwidth - new_overview_width - overviewoffset
         }])
         .toFile(generated_name);
     console.log(`generated ${generated_name}`);
